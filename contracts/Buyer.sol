c22857cf4e2451a8d390fd3ecb4d8595c6d3c67f
Muritala David <davmuri1414@gmail.com>
2021-08-09 08:40:23 -0700

    project token created

--- contracts/Buyer.sol
+++ contracts/Buyer.sol
@@ -2,6 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Buyer {
+
+  struct Person {
+    uint index;
+    string name;
+    bytes32 email;
+  }
+
+  mapping (address => Person) public persons;
+  address[] private personIndex;
   constructor() public {
   }
 }
--- contracts/Farmer.sol
+++ contracts/Farmer.sol
@@ -2,6 +2,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Farmer {
+
+  struct User {
+    uint index;
+    string name;
+    string location;
+    bytes32 email;
+  }
+
+  mapping (address => User) public users;
+  address[] private userIndex;
   constructor() public {
   }
 }
