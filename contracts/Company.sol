d3700f6cf51c527d503e8bc4db84ee1559e06c20
Muritala David <davmuri1414@gmail.com>
2021-08-09 08:24:46 -0700

    Farm struct

--- contracts/Company.sol
+++ contracts/Company.sol
@@ -2,6 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Company {
+  struct Farm {
+    uint index;
+    string name;
+    uint size;
+    uint256 price;
+    string location;
+    uint256 percentage;
+  }
   constructor() public {
   }
 }
--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -1,9 +1,13 @@
 const Buyer = artifacts.require("./Buyer.sol");
 const Company = artifacts.require("./Buyer.sol");
 const Farmer = artifacts.require("./Farmer.sol");
+const Token = artifacts.require("./Token.sol");
+const Exchange = artifacts.require("./Exchange.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(Buyer);
   deployer.deploy(Company);
   deployer.deploy(Farmer);
+  deployer.deploy(Token);
+  deployer.deploy(Exchange);
 };
